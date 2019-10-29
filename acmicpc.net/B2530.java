112e1c5d469ddc7d4d7dbdd4ad51bc9c2a6b50f0
Endki <khb3221@naver.com>
2019-10-29 20:52:39 +0900

    Create B2530.java

---
+++ acmicpc.net/B2530.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in) ;
+		
+		int hour=input.nextInt();
+		int min=input.nextInt();
+		int sec=input.nextInt();
+		int plussec=input.nextInt();
+		
+
+		sec+=plussec;
+		
+		min+=sec/60;
+		sec%=60;
+		
+		hour+=min/60;
+		min%=60;
+		
+		hour%=24;
+		System.out.println(hour+" "+min+" "+sec);
+
+	}
+
+}
