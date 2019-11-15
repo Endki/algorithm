60e7d4904efedf723a23543d85bc89f8f202146b
Endki <khb3221@naver.com>
2019-11-15 15:26:50 +0900

    Create B15894.java

---
+++ acmicpc.net/B15894.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class B15894 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input=new Scanner(System.in);
+		
+		long k=input.nextLong();
+		
+		System.out.print(k*4);
+		
+	}
+
+}
