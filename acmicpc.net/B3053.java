d3e30a33c8afd1015fa4e37c6f0fd46a0f1e4524
Endki <khb3221@naver.com>
2020-02-21 14:13:19 +0900

    200221

---
+++ acmicpc.net/B3053.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class B3053 {
+
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		
+		int r=input.nextInt();
+		
+		double ucle=Math.PI*(r*r);
+		double taxi=2*(r*r);
+		
+		System.out.println(String.format("%.6f", ucle));
+		System.out.println(String.format("%.6f", taxi));
+
+	}
+
+}
--- acmicpc.net/B4948.java
+++ acmicpc.net/B4948.java
@@ -3,39 +3,44 @@ import java.util.Scanner;
 public class B4948 {
 
 	public static void main(String[] args) {
-		Scanner input= new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
 
-		int count=0;
-		int tmp=0;
-		//기본 입력
-		while(true) {
-			int n=input.nextInt();
-			count=0;
-			
-			//n에서 2n까지 반복
-			for(int i=n;i<=2*n;i++) {
-				
-				
-				//소수 판별
-				for(int j=2;j<n;j++) {
-				
-					
-					//소수가 아님
-					if(n%j==0) {
-					 	
-					}else {
-						count++;
-					}
+		int[] arr = new int[246913];
+		arr[0] = 0;
+		arr[1] = 0;
+
+		for (int i = 2; i < 246913; i++) {
+			arr[i] = i;
+
+		}
+		
+		for (int i = 2; i < 246913; i++) {
+			for (int j = 1; i * j < 246913; j++) {
+				if (arr[i * j] /i != 1) {
+					arr[i * j] =0;
 				}
-				
+
 			}
-			
-			
-			 
-			
-			if(n==0) {
+
+		}
+
+		int count = 0;
+		int tmp = 0;
+		// 기본 입력
+		while (true) {
+			int n = input.nextInt();
+			if (n == 0) {
 				break;
 			}
+			
+			for(int i=n+1;i<=2*n;i++) {
+				if(arr[i]!=0) {
+					count++;
+				}
+			}
+			System.out.println(count);
+			count=0;
+
 		}
 	}
 
