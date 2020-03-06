f81f273489ea9e465c3e355359d649075366159b
Endki <khb3221@naver.com>
2020-03-06 21:58:44 +0900

    200306

--- acmicpc.net/B11509.java
+++ acmicpc.net/B11509.java
@@ -1,41 +1,29 @@
 import java.util.Scanner;
 
 public class B11509 {
-
-	static int count=0;
 	public static void main(String[] args) {
 		Scanner input=new Scanner(System.in);
 		
 		
-		int n=input.nextInt();
-		
-		
-		int count=0,temp=0;
-		int a=-1;
-		int arr[]=new int[n];
-		
+		int n=input.nextInt();		
+		int count=0;
+		int arr[]=new int[1000001];
+		int height=0;
 		for(int i=0;i<n;i++) {
+			height=input.nextInt();
 			
-			arr[i]=input.nextInt();			
 			
-			
+			if(arr[height+1]==0) {
+				count++;
+				arr[height]++;
+			}else {
+				arr[height+1]--;
+				arr[height]++;
+			}
 		}
-		temp=solve(arr,0);
-		System.out.println(temp);
-
-	}
-	
-	
-	public static int solve(int[] arr,int i) {
 		
-		if(arr[i]-arr[i+1]==1) {
-			solve(arr,i+1);
-			
-			return count+1;
-		}else {
-			solve(arr,i+1);
-		}
-		return count+1;
-	}
+		
+		System.out.println(count);
 
+	}
 }
---
+++ acmicpc.net/B1212.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class B1212 {
+
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+
+		String k =input.next();
+		int length=k.length();
+		
+		if(k=="0"&&length==1) {
+			System.out.println("0");
+		}
+		char[] k2=k.toCharArray();
+		
+		
+		System.out.print(last(k2[0]));
+		for(int i=1;i<length;i++) {
+			System.out.print(trans(k2[i]));
+		}
+		
+	}
+
+	public static String last(char a) {
+	    String result="";
+	    if (a == '1') result = "1";
+	    else if (a == '2') result = "10";
+	    else if (a == '3') result = "11";
+	    else if (a == '4') result = "100";
+	    else if (a == '5') result = "101";
+	    else if (a == '6') result = "110";
+	    else if (a == '7') result = "111";
+	    return result;
+	}
+	public static String trans(char a) {
+	    String result="";
+	    if (a == '0') result = "000";
+	    else if (a == '1') result = "001";
+	    else if (a == '2') result = "010";
+	    else if (a == '3') result = "011";
+	    else if (a == '4') result = "100";
+	    else if (a == '5') result = "101";
+	    else if (a == '6') result = "110";
+	    else if (a == '7') result = "111";
+	    return result;
+	}
+}
