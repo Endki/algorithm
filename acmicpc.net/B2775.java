904fd9aa92c839fb624335b18a46106c5cf1cb25
Endki <khb3221@naver.com>
2020-03-13 15:51:09 +0900

    200313

---
+++ acmicpc.net/B2775.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class B2775 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		int t = input.nextInt();
+
+		int sum = 0;
+		for (int i = 0; i < t; i++) {
+			sum = 0;
+			int k = input.nextInt();
+			int n = input.nextInt();
+			int[][] arr = new int[k + 1][n];
+
+			// 0Ãþ ÀÔ·Â
+			for (int j = 0; j < n; j++) {
+				arr[0][j] = j + 1;
+			}
+			
+			// À§Ãþ ÀÔ·Â
+			for (int j = 1; j <= k; j++) {
+				sum=0;
+				for (int l = 0; l < n; l++) {
+					sum += arr[j - 1][l];
+					arr[j][l] += sum;
+				}
+			}
+
+			System.out.println(arr[k][n - 1]);
+		}
+
+	}
+
+}
