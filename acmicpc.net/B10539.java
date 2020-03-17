1e9870b1d7d6dcebea3d57c1d40b3177205db2a5
Endki <khb3221@naver.com>
2020-03-17 21:09:48 +0900

    200317

---
+++ acmicpc.net/B10539.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class B10539 {
+
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		
+		int n=input.nextInt();
+		int[] arrA=new int[n+1];
+		int[] arrB=new int[n+1];
+		
+		for(int i=1;i<=n;i++) {
+			arrB[i]=input.nextInt();
+		}
+		
+		arrA[1]=arrB[1];
+		
+		System.out.print(arrA[1]);
+		
+		for(int i=2;i<=n;i++) {
+			arrA[i]=arrB[i]*i-arrB[i-1]*(i-1);
+			System.out.print(" "+arrA[i]);
+		}
+
+	}
+
+}
