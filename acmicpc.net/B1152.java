565aa5c3db98cec840689176ec6c17597b03b2bf
Endki <khb3221@naver.com>
2020-04-05 21:03:42 +0900

    200405

---
+++ acmicpc.net/B1152.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B1152 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stn=new StringTokenizer(br.readLine());
+		int count=stn.countTokens();
+//		while(stn.countTokens()) {
+//			count++;
+//		}
+//		
+		System.out.println(count);
+		
+	}
+
+}
---
+++ acmicpc.net/B2965.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class B2965 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		
+		int a=Integer.parseInt(st.nextToken());
+		int b=Integer.parseInt(st.nextToken());
+		int c=Integer.parseInt(st.nextToken());
+		
+		
+		if(c-b>b-a) {
+	
+		bw.write((c-b)-1+"\n");
+		}else {
+			bw.write((b-a)-1+"\n");
+		}
+		
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+	
+	
+	
+
+}
