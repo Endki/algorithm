b2ebf2a9fbd82224321104b1f8a89a7c77d997a9
Endki <khb3221@naver.com>
2020-04-20 22:19:23 +0900

    200420

---
+++ acmicpc.net/B2355.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B2355 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		
+		String[] k=br.readLine().split(" ");
+		long start=Long.parseLong(k[0]);
+		long end=Long.parseLong(k[1]);
+		
+		
+		if(start<end) {
+			
+			long temp=start;
+			start=end;
+			end=temp;
+		}
+		
+		System.out.println(((start-end+1)*(end+start))/2);
+	}
+
+}
--- acmicpc.net/B2839.java
+++ acmicpc.net/B2839.java
@@ -11,11 +11,21 @@ public class B2839 {
 		int count=0;
 		
 		
-		if(n>5) {
-			n%=5;
-			count++;
+		if(n%5==0) {
+			count=n/5;
+		}else if(n%5==1) {
+			count=n/5+1;
+		}else if(n%5==2 && n>=12) {
+			count=n/5+2;
+		}else if(n%5==3) {
+			count=n/5+1;
+		}else if(n%5==4 && n>=9) {
+			count=n/5+2;
+		}else {
+			count=-1;
 		}
 		
+		System.out.println(count);
 	}
 	
 
