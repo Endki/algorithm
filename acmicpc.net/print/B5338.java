c0c9a7bc7ab13e7d952bad01ea052236019bfd56
Endki <khb3221@naver.com>
2020-07-14 06:57:50 +0900

    200714

---
+++ acmicpc.net/print/B5338.java
@@ -0,0 +1,14 @@
+package print;
+
+public class B5338 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("       _.-;;-._");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-;;-._|");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-''-._|");
+	}
+
+}
