ed8372cc3f2f0974e8d639bf76116a5b212778e6
KSJ <khb3221@naver.com>
2020-09-20 10:42:55 +0900

    200920

---
+++ SWEA/src/tree/Test.java
@@ -0,0 +1,18 @@
+package tree;
+
+import java.util.Arrays;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer a =123;
+		
+		int [] arr=new int[1];
+		
+		arr[0]=a;
+		
+		System.out.println(Arrays.toString(arr));
+	}
+
+}
