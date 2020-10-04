1677ed4765493d0f57a7b41d297c6425a03feddd
KSJ <khb3221@naver.com>
2020-10-04 16:43:21 +0900

    201004

---
+++ programmers/src/문자열압축.java
@@ -0,0 +1,25 @@
+
+public class 문자열압축 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+    static public int solution(String s) {
+        int answer = s.length();
+        
+        for(int i=1;i<=s.length()/2;i++) {
+        	StringBuilder sb=new StringBuilder();
+        	
+        	
+        	for(int j=0;j<s.length();j=j+i) {
+        		
+        	}
+        }
+        
+        
+        return answer;
+    }
+	
+}
