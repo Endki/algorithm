5b01f49346004c0885e36ff58006b999eb8b46cc
KSJ <khb3221@naver.com>
2020-11-19 13:20:09 +0900

    Create P62048.java

---
+++ programmers/src/P62048.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class P62048 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(solution(8, 12));
+	}
+	public static long solution(int w,int h) {
+		long answer=1;
+		//대각선 = 최단 경로 문제 0,0 에서 w,h까지의 최단 경로
+		
+		boolean[][] paper=new boolean[h][w];
+		
+		
+		
+		
+		return answer;
+	}
+}
