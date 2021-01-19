620f9334b036dfd1ab9d51964e7248fd769012d4
KSJ <khb3221@naver.com>
2021-01-19 19:17:07 +0900

    Create B10216.java

---
+++ acmicpc.net/src/B10216.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B10216 {
+
+	static int[][] map;
+	static boolean[][] visited;
+	//8방 탐색
+	static int dx[]= {-1,-1,-1,0,0,1,1,1};
+	static int dy[]= {-1,0,1,-1,1,-1,0,1};
+	
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		
+		int TC=Integer.parseInt(br.readLine());
+		
+		while(TC-->0) {
+			map=new int[5001][5001];
+			int K=Integer.parseInt(br.readLine());
+			
+			while(K-->0) {
+				
+			}
+		}
+		
+		
+	}
+
+}
