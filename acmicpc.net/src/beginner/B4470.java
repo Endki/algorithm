c3c3d9f467fbdf2c2640314e8579d47b42a5968e
KSJ <khb3221@naver.com>
2021-07-01 22:28:41 +0900

    Create B4470.java

---
+++ acmicpc.net/src/beginner/B4470.java
@@ -0,0 +1,23 @@
+package beginner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B4470 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		int N=Integer.parseInt(br.readLine());
+		
+		for(int i=1;i<=N;i++) {
+			String k=br.readLine();
+			
+			System.out.println(i+". "+k);
+		}
+
+	}
+
+}
