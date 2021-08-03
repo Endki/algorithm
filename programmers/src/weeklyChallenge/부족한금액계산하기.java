c341797667b88bca6303c933d986c24c928f9904
KSJ <khb3221@naver.com>
2021-08-03 17:02:15 +0900

    Create 부족한금액계산하기.java

---
+++ programmers/src/weeklyChallenge/부족한금액계산하기.java
@@ -0,0 +1,29 @@
+package weeklyChallenge;
+
+import java.io.IOException;
+
+public class 부족한금액계산하기 {
+
+	public static void main(String[] args) throws IOException{
+		System.out.println(solution(2500, 20, 2500));
+
+	}
+
+	public static long solution(int price, int money, int count) {
+		
+		long sum=0;
+		
+		for(int i=1;i<=count;i++) {
+			sum+=price*i;
+		}
+		
+		if(sum<=money) {
+			return 0;
+		}else {
+			return sum-money;
+		}
+		
+		
+		
+	}
+}
