04540a23875274cbe0ecbb7f0baca6214b20ac04
KSJ <khb3221@naver.com>
2021-08-04 16:50:15 +0900

    Create B5032.java

---
+++ acmicpc.net/src/math/B5032.java
@@ -0,0 +1,28 @@
+package math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B5032 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer stz=new StringTokenizer(br.readLine());
+		
+		int e=Integer.parseInt(stz.nextToken());
+		int f=Integer.parseInt(stz.nextToken());
+		int c=Integer.parseInt(stz.nextToken());
+		
+		
+		int answer=0;
+		
+		while(!(e<c)) {
+			
+		}
+
+	}
+
+}
