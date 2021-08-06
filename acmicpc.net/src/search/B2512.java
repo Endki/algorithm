2ddbf1678ca79acfed4345cf0575b8002b5bdbab
KSJ <khb3221@naver.com>
2021-08-06 20:07:35 +0900

    Create B2512.java

---
+++ acmicpc.net/src/search/B2512.java
@@ -0,0 +1,16 @@
+package search;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B2512 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		
+		
+
+	}
+
+}
