625724eb4d73099fca494c921f1038306bade0e9
KSJ <khb3221@naver.com>
2021-08-12 19:21:47 +0900

    Update P81301.java

--- programmers/src/lv1/P81301.java
+++ programmers/src/lv1/P81301.java
@@ -11,11 +11,6 @@ public class P81301 {
 	public static int solution(String s) {
 		
 		s=s.replace("one", "1").replace("zero", "0").replace("two", "2").replace("three", "3").replace("four", "4").replace("five", "5").replace("six", "6").replace("seven", "7").replace("eight", "8").replace("nine", "9");
-		
-		
-		
-		
-		
 		return Integer.parseInt(s);
 	}
 }
