a78f209c0fc5b7b442bb01be56743e735c80dc86
KSJ <khb3221@naver.com>
2021-08-12 19:07:03 +0900

    Create P81301.java

---
+++ programmers/src/lv1/P81301.java
@@ -0,0 +1,21 @@
+package lv1;
+
+public class P81301 {
+
+	public static void main(String[] args) {
+		
+		System.out.println(solution("one4seveneight"));
+	}
+	
+	
+	public static int solution(String s) {
+		
+		s=s.replace("one", "1").replace("zero", "0").replace("two", "2").replace("three", "3").replace("four", "4").replace("five", "5").replace("six", "6").replace("seven", "7").replace("eight", "8").replace("nine", "9");
+		
+		
+		
+		
+		
+		return Integer.parseInt(s);
+	}
+}
