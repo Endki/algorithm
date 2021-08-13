288d1d9fd2f4fcc05cac23c34bf7b399f65edd36
KSJ <khb3221@naver.com>
2021-08-13 15:11:06 +0900

    Create P76501.java

---
+++ programmers/src/lv1/P76501.java
@@ -0,0 +1,23 @@
+package lv1;
+
+public class P76501 {
+
+	public static void main(String[] args) {
+		
+		System.out.println(solution(new int[] {4,7,12}, new boolean[] {true,false,true}));
+	}
+
+    public static int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        
+        for(int i=0;i<absolutes.length;i++) {
+        	if(signs[i]) {
+        		answer+=absolutes[i];
+        	}else {
+        		answer-=absolutes[i];
+        	}
+        }
+        
+        return answer;
+    }
+}
