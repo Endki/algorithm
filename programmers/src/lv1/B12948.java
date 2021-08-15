72f1a5371b79c17f911be0c7194821412838b1a7
KSJ <khb3221@naver.com>
2021-08-15 16:33:19 +0900

    Create B12948.java

---
+++ programmers/src/lv1/B12948.java
@@ -0,0 +1,23 @@
+package lv1;
+
+public class B12948 {
+
+	public static void main(String[] args) {
+
+		System.out.println(solution("027778888"));
+	}
+    public static String solution(String phone_number) {
+        
+        
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<phone_number.length();i++) {
+        	if(i>=phone_number.length()-4) {
+        		sb.append(phone_number.charAt(i));
+        	}else {
+        		sb.append("*");
+        	}
+        }
+        
+        return sb.toString();
+    }
+}
