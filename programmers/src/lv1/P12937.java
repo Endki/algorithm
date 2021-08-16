d3f158d417ec1cf3ed2e587850fa7275b5aeb61f
KSJ <khb3221@naver.com>
2021-08-16 14:22:06 +0900

    Create P12937.java

---
+++ programmers/src/lv1/P12937.java
@@ -0,0 +1,17 @@
+package lv1;
+
+public class P12937 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+    public static String solution(int num) {
+        
+        if(num%2==1) {
+        	return "Odd";
+        }
+        
+        return "Even";
+    }
+}
