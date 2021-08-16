82c6aad076be14adcc87763f45b68bb513bd5697
KSJ <khb3221@naver.com>
2021-08-16 14:10:00 +0900

    Create P12933.java

---
+++ programmers/src/lv1/P12933.java
@@ -0,0 +1,32 @@
+package lv1;
+
+import java.util.Arrays;
+
+public class P12933 {
+
+	public static void main(String[] args) {
+		System.out.println(solution(118372));
+
+	}
+    public static long solution(long n) {
+        long answer = 0;
+        String input=String.valueOf(n);
+        int[] arr=new int[input.length()];
+        
+        
+        for(int i=0;i<input.length();i++) {
+        	arr[i]=input.charAt(i)-'0';
+        }
+        
+        Arrays.sort(arr);
+        StringBuilder sb=new StringBuilder();
+        for(int i=arr.length-1;i>-1;i--) {
+        	sb.append(arr[i]);
+        }
+        
+        
+        
+        
+        return Long.parseLong(sb.toString());
+    }
+}
