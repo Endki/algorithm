7c28c33c731ce37cd5ef1b9ebd8e558569b54311
KSJ <khb3221@naver.com>
2021-08-16 14:25:15 +0900

    Update P12937.java

--- programmers/src/lv1/P12937.java
+++ programmers/src/lv1/P12937.java
@@ -3,11 +3,16 @@ package lv1;
 public class P12937 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println(solution(0));
 
 	}
     public static String solution(int num) {
         
+    	if(num<0) {
+    		num=num*-1;
+    	}
+    	
+    	
         if(num%2==1) {
         	return "Odd";
         }
