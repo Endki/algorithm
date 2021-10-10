438dde6149012f31647670e2b1bcbcbc6b74e4cf
KSJ <khb3221@naver.com>
2021-10-10 21:52:02 +0900

    Update FindNumberswithEvenNumber.java

--- leetcode/src/arrays101/FindNumberswithEvenNumber.java
+++ leetcode/src/arrays101/FindNumberswithEvenNumber.java
@@ -3,13 +3,8 @@ package arrays101;
 public class FindNumberswithEvenNumber {
     public int findNumbers(int[] nums) {
         int answer=0;
-        
-        
-        
+
         for(int i=0;i<nums.length;i++){
-        	
-        	
-        	
             if(isCheckEven(Integer.toString(nums[i]).length())){
                 answer++;
             }
