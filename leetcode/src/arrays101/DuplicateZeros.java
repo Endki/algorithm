93b84db690b3763000cdea31400dfce1f235f890
KSJ <khb3221@naver.com>
2021-10-13 23:15:25 +0900

    Update DuplicateZeros.java

--- leetcode/src/arrays101/DuplicateZeros.java
+++ leetcode/src/arrays101/DuplicateZeros.java
@@ -1,5 +1,23 @@
 package arrays101;
 
-public class DuplicateZeros {
+import java.util.ArrayList;
 
+public class DuplicateZeros {
+    public void duplicateZeros(int[] arr) {
+        ArrayList<Integer> solve=new ArrayList<Integer>();
+        
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==0){
+                    solve.add(0);
+                    solve.add(0);
+            }else{
+                solve.add(arr[i]);
+            }
+        }
+        
+
+        for(int i=0;i<arr.length;i++){
+            arr[i]=solve.get(i);
+        }
+    }
 }
