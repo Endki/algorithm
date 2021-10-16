f0fd6afbaccfd5a316e6d0a943b2ad9e64b6fe2c
KSJ <khb3221@naver.com>
2021-10-16 20:27:24 +0900

    Create RemoveDuplicatesFromSortedArray.java

---
+++ leetcode/src/arrays101/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,35 @@
+package arrays101;
+
+public class RemoveDuplicatesFromSortedArray {
+    public int removeDuplicates(int[] nums) {
+        boolean[] list=new boolean[201];
+        
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<0){
+                list[Math.abs(nums[i])+100]=true;
+            }else{
+                list[nums[i]]=true;
+            }
+        }
+        ArrayList<Integer> output=new ArrayList<Integer>();
+        
+        for(int i=200;i>100;i--){
+            if(list[i]){
+                output.add((i-100)*-1);
+            }
+        }
+        
+        for(int i=0;i<101;i++){
+            if(list[i]){
+                output.add(i);
+            }
+        }
+     
+        for(int i=0;i<output.size();i++){
+            nums[i]=output.get(i);
+        }
+        
+        
+        return output.size();
+    }
+}
