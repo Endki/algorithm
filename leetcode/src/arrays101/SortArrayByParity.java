e510cc50aae009ad9edd3e2170a024fbd89d4cfd
KSJ <khb3221@naver.com>
2021-10-19 18:29:19 +0900

    Create SortArrayByParity.java

---
+++ leetcode/src/arrays101/SortArrayByParity.java
@@ -0,0 +1,28 @@
+package arrays101;
+
+public class SortArrayByParity {
+    public int[] sortArrayByParity(int[] nums) {
+        ArrayList<Integer> odd=new ArrayList<Integer>();
+        ArrayList<Integer> even=new ArrayList<Integer>();
+        
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]%2==0){
+                even.add(nums[i]);
+            }else{
+                odd.add(nums[i]);
+            }
+        }
+        
+        
+        for(int i=0;i<even.size();i++){
+            nums[i]=even.get(i);
+        }
+        
+        int eSize=even.size();
+        for(int i=0;i<odd.size();i++){
+            nums[i+eSize]=odd.get(i);
+        }
+        
+        return nums;
+    }
+}
