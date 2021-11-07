0a33802fe6701d2dca5a6c1d0c92b3d11d46ff6d
KSJ <khb3221@naver.com>
2021-11-07 20:24:51 +0900

    Create RotateArray.java

---
+++ leetcode/src/topInterviewQuestions/Array/RotateArray.java
@@ -0,0 +1,14 @@
+package topInterviewQuestions.Array;
+
+public class RotateArray {
+    public void rotate(int[] nums, int k) {
+        int[] solve=new int[nums.length];
+        
+        for(int i=0;i<nums.length;i++){
+            solve[(i+k)%nums.length]=nums[i];
+        }
+        for(int i=0;i<nums.length;i++){
+            nums[i]=solve[i];
+        }
+    }
+}
