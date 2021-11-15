419c8c59f53df22548775013308bd9af380f55e6
KSJ <khb3221@naver.com>
2021-11-15 20:35:25 +0900

    Update ImplementstrStar.java

--- leetcode/src/topInterviewQuestions/Strings/ImplementstrStar.java
+++ leetcode/src/topInterviewQuestions/Strings/ImplementstrStar.java
@@ -51,17 +51,31 @@ public class ImplementstrStar {
     
     //정답 코드
     public int strStrSolve(String haystack, String needle) {
+    	
+    	//니들이 더 크면 계산할 필요없음
         if(haystack.length() < needle.length())return -1;
+        
+        	//니들이 없으면 무조건 0임
            if(needle.length() == 0)return 0;
+           
+           
            for(int i = 0;i< haystack.length();i++){
+        	   
+        	   //예외처리
                if(i + needle.length() > haystack.length())break;
+               
+               //첫글자와 끝 글자가 같으면 로직 실행
                if(haystack.charAt(i) == needle.charAt(0) && haystack.charAt(i + needle.length() - 1) == needle.charAt(needle.length() - 1)){
+            	   
+            	   
                    for(int j = 0;j< needle.length();j++){
                        if(haystack.charAt(i+j) != needle.charAt(j))break;
                        if(j == needle.length()-1)return i;
                    }
                }
            }
+           
+           
            return -1;
        }
     
