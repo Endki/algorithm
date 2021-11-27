e30147a6b7d934d54df055393bc6606d6817f276
KSJ <khb3221@naver.com>
2021-11-27 21:25:51 +0900

    Create SingleNumber.java

---
+++ leetcode/src/topInterviewQuestions/Array/SingleNumber.java
@@ -0,0 +1,14 @@
+package topInterviewQuestions.Array;
+
+public class SingleNumber {
+
+	public int singleNumber(int[] nums) {
+		int ans = 0;
+
+		int len = nums.length;
+		for (int i = 0; i != len; i++)
+			ans ^= nums[i];
+
+		return ans;
+	}
+}
