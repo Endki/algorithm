ed8486465422b237956b9c0680c9ee9b9a0d186c
KSJ <khb3221@naver.com>
2021-11-28 18:10:31 +0900

    Create B4101.java

---
+++ acmicpc.net/src/beginner/B4101.java
@@ -0,0 +1,31 @@
+package beginner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B4101 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		
+		while(true) {
+			StringTokenizer stz=new StringTokenizer(br.readLine());
+			int a=Integer.parseInt(stz.nextToken());
+			int b=Integer.parseInt(stz.nextToken());
+			if(a==0 && b==0) {
+				break;
+			}
+			
+			
+			if(b<a) {
+				System.out.println("YES");
+			}else {
+				System.out.println("NO");
+			}
+		}
+
+	}
+
+}
