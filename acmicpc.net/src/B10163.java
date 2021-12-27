29af04da7dc59b9401b448b01aa535de106fb042
KSJ <khb3221@naver.com>
2021-12-27 18:32:22 +0900

    Create B10163.java

---
+++ acmicpc.net/src/B10163.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B10163 {
+
+	public static void main(String[] args)throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		
+		int N=Integer.parseInt(br.readLine());
+		
+		for(int i=0;i<N;i++) {
+			
+		}
+	}
+
+}
