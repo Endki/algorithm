87e21d06452c8d696d1b74c31b580c9f3fbd386a
KSJ <khb3221@naver.com>
2021-12-27 18:32:16 +0900

    Update B1713.java

--- acmicpc.net/src/simulation/B1713.java
+++ acmicpc.net/src/simulation/B1713.java
@@ -3,7 +3,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class B1713 {
