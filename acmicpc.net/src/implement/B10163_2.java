ae148045feec2393749da3144161fdfdb7207248
KSJ <khb3221@naver.com>
2021-12-27 20:59:49 +0900

    Update B10163_2.java

--- acmicpc.net/src/implement/B10163_2.java
+++ acmicpc.net/src/implement/B10163_2.java
@@ -1,9 +1,10 @@
+package implement;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class B10163 {
+public class B10163_2 {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
