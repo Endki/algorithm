61eca5c9e0b897b04cec130c37cd93b90271e194
KSJ <khb3221@naver.com>
2022-01-22 18:35:52 +0900

    Create P12900.java

---
+++ programmers/src/P12900.java
@@ -0,0 +1,19 @@
+
+public class P12900 {
+    public int solution(int n) {
+
+        int a=1;
+        int b=1;
+        
+        
+        for(int i=0;i<n-1;i++){
+            int c =(a+b)%1000000007;
+            a=b;
+            b=c;
+            
+        }
+        
+        
+        return b;
+    }
+}
