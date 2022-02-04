bbc324e7e8cf795554aa08fdd839d3cffbd484d7
KSJ <khb3221@naver.com>
2022-02-04 18:19:16 +0900

    Create P1832.java

---
+++ programmers/src/lv3/P1832.java
@@ -0,0 +1,32 @@
+package lv3;
+
+public class P1832 {
+
+    int MOD = 20170805;
+    public int solution(int m, int n, int[][] cityMap) {
+        int answer = 0;
+        
+        int[][][] dp=new int[m+1][n+1][2];
+        dp[1][1][0]=1;
+        dp[1][1][1]=1;
+        //모든 경로 확인
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                
+                if(cityMap[i-1][j-1]==0){
+                    dp[i][j][0]+=(dp[i-1][j][0]+dp[i][j-1][1])%MOD;
+                    dp[i][j][1]+=(dp[i-1][j][0]+dp[i][j-1][1])%MOD;
+                }else if(cityMap[i-1][j-1]==1){
+                    dp[i][j][0]=0;
+                    dp[i][j][1]=0;
+                }else{
+                    dp[i][j][0]=dp[i-1][j][0]%MOD;
+                    dp[i][j][1]=dp[i][j-1][1]%MOD;
+                }
+            }
+        }
+        
+        
+        return dp[m][n][0];
+    }
+}
