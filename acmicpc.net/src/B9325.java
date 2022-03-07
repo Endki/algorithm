b334548072bcf5b48f0c8c44df8a43dea3b14124
KSJ <khb3221@naver.com>
2022-03-07 16:11:47 +0900

    Create B9325.java

---
+++ acmicpc.net/src/B9325.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B9325 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		
+		int T=Integer.parseInt(br.readLine());
+		
+		
+		while(T-->0) {
+			int base=Integer.parseInt(br.readLine());
+			
+			int optionCount=Integer.parseInt(br.readLine());
+			
+			while(optionCount-->0) {
+				String[] option=br.readLine().split(" ");
+				
+				base+=Integer.parseInt(option[0])*Integer.parseInt(option[1]);
+			}
+			
+			System.out.println(base);
+		}
+		
+	}
+
+}
